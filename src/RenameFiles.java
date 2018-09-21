cbfc8fcc49681e82810f86da54fd9259357fa4d9
shihne <shihne@mail.ru>
2018-09-21 12:28:14 +0300

    Add Task3

---
+++ src/RenameFiles.java
@@ -0,0 +1,10 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RenameFiles {
+    public static void main(String[] args) {
+        Path pa = Paths.get("Folder for Task3/a.txt");
+        Path pb = Paths.get("Folder for Task3/b.txt");
+
+    }
+}
