59af7aadd670bfcc214be6a213d5c9aaaf76ab70
st064329 <shihne@mail.ru>
2018-10-26 11:04:15 +0300

    Задача 2 по JavaFx. Сделано до 6

--- src/ru/spbu/arts/javafx/CreateCircle.java
+++ src/ru/spbu/arts/javafx/CreateCircle.java
@@ -1,14 +1,19 @@
 package ru.spbu.arts.javafx;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import java.util.Map;
 
 public class CreateCircle extends Application {
 
@@ -16,6 +21,8 @@ public class CreateCircle extends Application {
     private ColorPicker cp1 = new ColorPicker();
     private ColorPicker cp2 = new ColorPicker();
     private Circle circle = new Circle();
+    private Pane pane = new Pane();
+    private VBox panel = new VBox();
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Chat JavaFX Application");
@@ -31,19 +38,66 @@ public class CreateCircle extends Application {
     private Parent initInterface() {
         HBox root = new HBox();
 
-        VBox panel = new VBox();
-        VBox result = new VBox();
 
-        root.getChildren().addAll(panel, result);
 
-        panel.getChildren().addAll(slider, cp1, cp2);
-        result.getChildren().add(circle);
+        root.getChildren().addAll(panel, pane);
+
+        Label radius = new Label("Выберите радиус");
+        Label circle_color = new Label("Выберите цвет круга");
+        Label bgColor = new Label("Выберите цвет фона");
+        panel.getChildren().addAll(radius, slider, circle_color, cp1, bgColor, cp2);
+        slider.setMin(0);
+        slider.setMax(100);
+        slider.setValue(50);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        cp2.setValue(Color.CORAL);
+        pane.getChildren().add(circle);
+        HBox.setHgrow(pane, Priority.ALWAYS);
         panel.setPrefWidth(300);
+        pane.setPrefWidth(300);
 
         return root;
     }
 
     private void initInteraction() {
+        circle.radiusProperty().bind(
+                slider.valueProperty()
+        );
+        circle.fillProperty().bind(
+                cp1.valueProperty()
+        );
 
+        //TODO используйте связывание вместо слушателей
+
+        pane.backgroundProperty().bind(
+                Bindings.createObjectBinding(
+                        () -> new Background(new BackgroundFill(cp2.getValue(), new CornerRadii(0), Insets.EMPTY)),
+                        cp2.valueProperty()
+                )
+        );
+
+        /*panel.heightProperty().addListener(
+                prop -> circle.setCenterY(panel.getHeight() / 2)
+        );
+        panel.widthProperty().addListener(
+                prop -> circle.setCenterX(panel.getWidth() / 2)
+        );
+
+        circle.setCenterX(panel.getWidth() / 2);
+        circle.setCenterY(panel.getHeight() / 2);*/
+
+        circle.centerYProperty().bind(
+                Bindings.createDoubleBinding(
+                        () -> panel.getHeight() / 2,
+                        panel.heightProperty()
+                )
+        );
+        circle.centerXProperty().bind(
+                Bindings.createDoubleBinding(
+                        () -> pane.getWidth() / 2,
+                        pane.widthProperty()
+                )
+        );
     }
 }
