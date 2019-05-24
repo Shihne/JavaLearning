5705dbf9699542e6acffcd6011a28de24329ba12
st064329 <>
2019-05-24 10:17:23 +0300

    Исправлена часть косяков прошлого коммита и пуша

--- src/ru/spbu/arts/svg/Shape/Shape.java
+++ src/ru/spbu/arts/svg/Shape/Shape.java
@@ -1,12 +1,8 @@
 package ru.spbu.arts.svg.Shape;
 
-<<<<<<< HEAD
-public interface Shape {
 
-=======
 import ru.spbu.arts.svg.SVG;
 import ru.spbu.arts.svg.Tag;
-
 import java.util.List;
 
 public interface Shape {
@@ -14,10 +10,8 @@ public interface Shape {
     List<Tag> getTags();
 
     default void draw(SVG svg) {
-        for (Tag tag : new SmallSquare().getTags())
+        for (Tag tag : this.getTags())
             svg.addTag(tag);
-        for (Tag tag : new RedCircle().getTags())
-            svg.addTag(tag);
-    }
->>>>>>> 1dc9cf5e40a9b793d5a3c695ba64481163d35b06
+            }
+
 }
--- src/ru/spbu/arts/svg/Shape/ShapesDrawing.java
+++ src/ru/spbu/arts/svg/Shape/ShapesDrawing.java
@@ -2,10 +2,25 @@ package ru.spbu.arts.svg.Shape;
 
 import ru.spbu.arts.svg.SVG;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class ShapesDrawing {
     public static void main(String[] args) {
-        try {
-            SVG svg = new SVG("d.svg", 300, 300);
+        try (
+            SVG svg = new SVG("d.svg", 400, 400))
+            {
+            Random r = new Random();
+            List<Shape> shapes = new ArrayList<>();
+            for (int i = 0; i < 100; i++)
+                shapes.add(new PositionedShape(new SmallSquare(), r.nextInt(400), r.nextInt(400)));
+            for (int i = 0; i < 150; i++)
+                shapes.add(new PositionedShape(new RedCircle(), r.nextInt(400), r.nextInt(400)));
+            Collections.shuffle(shapes);
+            for (Shape shape : shapes)
+                shape.draw(svg);
 
         } catch (Exception e) {
             e.printStackTrace();
